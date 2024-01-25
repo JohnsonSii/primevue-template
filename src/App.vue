eef63cdfdf3fa30b73ae27ba5d5c4679bb2c5050
JohnsonSii <2933764599@qq.com>
2024-01-26 02:08:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+const body = document.querySelector('body')
+body.style.margin = '0'
+body.style.padding = '0'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-content-center bg-orange-400">
+    <Button label="Small" icon="pi pi-check" size="small" />
+  </div>
+  <div class="flex justify-content-center bg-orange-400">
+    <Button label="Large" icon="pi pi-check" size="large" />
+  </div>
+</template>
+
+<script setup>
+import Button from 'primevue/button'
+</script>
+
+<style scoped>
+@import 'primevue/resources/themes/lara-light-blue/theme.css';
+</style>
